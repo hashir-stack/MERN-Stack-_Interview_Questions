02100c57c52fea98499353cd034549d449617348
hashir-stack <hashir7ansari@gmail.com>
2026-02-25 21:32:31 +0530

    components of a CSS box model

---
+++ CSS.js
@@ -0,0 +1,20 @@
+// What are the components of a CSS box model ?
+
+// Content -> The actual text, image, or other data inside the element.
+// "Hello World" inside a <div>
+
+// Padding -> Space between the content and the element’s border. It pushes the border outward.
+
+// Border -> The line surrounding the padding and content. Can have thickness, style, and color.
+
+// Margin -> Space outside the border, creating distance between this element and others.
+
+// Visual Order (from inside out)
+// - Content → the core (text, image, etc.)
+// - Padding → space around the content
+// - Border → wraps around padding + content
+// - Margin → space outside the border
+
+// This means the total width of the box =  content width + padding + border + margin.
+
+// -----------------------------------------------------------------------------------------------------------------
