350df4c08980c36a8b25e112e713620eee803014
hashir-stack <hashir7ansari@gmail.com>
2026-03-04 22:08:45 +0530

    Virtual DOM

---
+++ React.js
@@ -0,0 +1,8 @@
+//1 Explain Virtual DOM , and how its work ?
+
+// The Virtual DOM is a lightweight, in-memory copy of the real DOM (Document Object Model). It is a JavaScript representation of the actual webpage structure that allows frameworks like React to update the UI more efficiently.
+// Instead of changing the real DOM directly (which is slow), changes are first applied to the Virtual DOM. Then React compares the new Virtual DOM with the old one, finds the differences, and updates only the necessary parts of the real DOM.
+// 👉 In short:
+// Virtual DOM = A fast, virtual copy of the real DOM that helps update webpages quickly and efficiently
+
+
